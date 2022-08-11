c7d6daf4836d0a7ab625157d5aa1589557c7e608
nokv <naoki.tkb@gmail.com>
2022-08-11 18:19:38 +0900

    chore: update

--- src/vue-shim.d.ts
+++ src/vue-shim.d.ts
@@ -4,14 +4,3 @@ declare module '*.vue' {
     const component: ReturnType<typeof defineComponent>;
     export default component;
 }
-
-// declare module '*.vue' {
-//     import type { DefineComponent } from 'vue';
-//     const component: DefineComponent<[], [], []>;
-//     export default component;
-// }
-
-// declare module '*.vue' {
-//     import Vue from 'vue';
-//     export default Vue;
-// }
