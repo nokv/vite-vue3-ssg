00bcd70df0c18b80d8968248a5177a001c41703e
nokv <naoki.tkb@gmail.com>
2022-08-16 12:56:47 +0900

    update: output html config
    format: minify

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
     ssgOptions: {
         dirStyle: 'nested',
         script: 'async',
+        formatting: 'minify',
     },
     build: {
         cssCodeSplit: true,
